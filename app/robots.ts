ba45efa3f1958b516888d1b8cde12de5a7d713f6
DragAdi <93391458+DragAditya@users.noreply.github.com>
2024-11-25 22:29:04 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://adithyakrishnan.com/sitemap.xml',
+    sitemap: 'https://dragadi.com/sitemap.xml',
   };
 }
